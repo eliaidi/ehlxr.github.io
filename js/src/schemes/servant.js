f685288fc03c7172dcd66e2b8435405ec55ab593
ehlxr <lxreh3@gmail.com>
2016-10-13 12:08:35 +0800

    站点更新: 2016-10-13 12:08:33

--- js/src/schemes/servant.js
+++
@@ -1,1 +0,0 @@
-$(document).on("motion:before",function(){NexT.motion.middleWares.sidebar=function(e){$(".sidebar .motion-element").velocity("transition.slideDownIn",{stagger:50,drag:!0,complete:function(){e.next()}})}});
